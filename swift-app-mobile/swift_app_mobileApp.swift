d001b9f27e249c8e89abb01cbdf082ffdd94d239
linh <linhhtk@tekup.vn>
2026-03-03 10:59:00 +0700

    Initial Commit

---
+++ swift-app-mobile/swift_app_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_app_mobileApp.swift
+//  swift-app-mobile
+//
+//  Created by Kieu Linh on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_app_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-app-mobileTests/swift_app_mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_app_mobileTests.swift
+//  swift-app-mobileTests
+//
+//  Created by Kieu Linh on 3/3/26.
+//
+
+import Testing
+@testable import swift_app_mobile
+
+struct swift_app_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
